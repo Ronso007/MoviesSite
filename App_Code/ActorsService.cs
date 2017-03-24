bf31ddf003c95727032cf5e34baa0caa93d03f34
Ron soffer <ronso008@Gmail.com>
2017-03-24 21:31:13 +0300

    s

---
+++ App_Code/ActorsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ActorsService
+/// </summary>
+public class ActorsService
+{
+    protected OleDbConnection myConn;
+    OleDbParameter objParam;
+
+    public ActorsService()
+    {
+        myConn = new OleDbConnection(Connect.getConnectionString());
+
+    }
+
+    public void InsertActor(ActorsDetails actor)
+    {
+
+    }
+}
