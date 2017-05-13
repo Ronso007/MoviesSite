714491c41a23f30b4d08c833edc04b43fe085b40
Ron soffer <ronso008@Gmail.com>
2017-05-13 16:07:16 +0300

    asd

---
+++ App_Code/Services/RatingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RatingService
+/// </summary>
+public class RatingService
+{
+    public RatingService()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
