a5ed5e4c4e38435765020ee82c536b6921c7e1e7
Ronso007 <ronso008@gmail.com>
2017-05-17 08:32:26 +0300

    Add files via upload

---
+++ App_Code/WebServiceImdb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebServiceImdb
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebServiceImdb : System.Web.Services.WebService {
+
+    public WebServiceImdb () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+    [WebMethod]
+    public MoviesDetails GetMovieByID(int id)
+    {
+        MoviesService service = new MoviesService();
+        return service.GetMovieByID(id);
+    }
+}
